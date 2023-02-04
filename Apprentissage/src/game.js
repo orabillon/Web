986b3de92c2c3fc5769e21adbb0c9314d474ad7c
orabillon <115501074+orabillon@users.noreply.github.com>
2023-02-04 16:10:27 +0100

    Ajout dt + clavier

--- Apprentissage/src/game.js
+++ Apprentissage/src/game.js
@@ -1,12 +1,68 @@
 let img;
 let monSprite;
 
+let KeyRight = false;
+let KeyLeft = false;
+let KeyDown = false;
+let KeyUp = false;
+
+let speed = 3;
+
+function toucheEnfoncee(t){
+    t.preventDefault();
+
+    if (t.code == "ArrowLeft"){
+        KeyLeft = true;
+    }
+    if (t.code == "ArrowRight"){
+        KeyRight = true;
+    }
+    if (t.code == "ArrowUp"){
+        KeyUp = true;
+    }
+    if (t.code == "ArrowDown"){
+        KeyDown = true;
+    }
+}
+
+function toucheRelachee(t){
+    t.preventDefault();
+
+    if (t.code == "ArrowLeft"){
+        KeyLeft = false;
+    }
+    if (t.code == "ArrowRight"){
+        KeyRight = false;
+    }
+    if (t.code == "ArrowUp"){
+        KeyUp = false;
+    }
+    if (t.code == "ArrowDown"){
+        KeyDown = false;
+    }
+}
+
 function load(){
+    // écouteur d'événement
+    document.addEventListener("keydown", toucheEnfoncee, false);
+    document.addEventListener("keyup", toucheRelachee, false);
+
     monSprite = new Sprite("../image/ship.png", 100, 45);
 }
 
-function update(){
-
+function update(dt){
+    if (KeyRight){
+        monSprite.x += speed;
+    }
+    if (KeyLeft){
+        monSprite.x -= speed;
+    }
+    if (KeyUp){
+        monSprite.y -= speed;
+    }
+    if (KeyDown){
+        monSprite.y += speed;
+    }
 }
 
 function draw(pCtx){
--- Apprentissage/src/main.js
+++ Apprentissage/src/main.js
@@ -2,17 +2,39 @@ let interval;
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
+let dernierUpdate = 0;
+let fps = 0;
 
-function run(){
-    update();
+function showFPS(){
+    ctx.fillStyle = "white";
+    ctx.font = "normal 16pt Arial";
+
+    ctx.fillText(Math.floor(fps) + "  FPS ", 10, 20);
+}
+
+function run(time){
+    requestAnimationFrame(run);
+   // let maintenant = Date.now();
+    let dt = (time - dernierUpdate) / 1000;
+
+    if ( dt < (1 / 71) - 0.001){
+        return;
+    }
+
+    dernierUpdate = time;
+    fps = 1 / dt;
+
+    update(dt);
     ctx.clearRect(0,0,canvas.width,canvas.height)
     draw(ctx);
+    showFPS();
 }
 
 function init(){
     console.log("init");
     load();
-    interval = setInterval(run, 1000 / 60);
+   // interval = setInterval(run, 1000 / 60);
+    requestAnimationFrame(run);
 }
 
 init();
